aec85e52e55631f4dae1b9cff26c5b620c53b14c
agung <agungpermadi9@gmail.com>
2024-12-16 16:37:29 +0700

    control flow dan perulangan

--- golang-dasar/anonymous_function.go
+++ golang-dasar/anonymous_function.go
@@ -1,1 +1,24 @@
 package main
+
+import "fmt"
+
+type BandUserFilter func(name string) bool
+
+func registerUserBanned(nameUser string, bandUserFilter BandUserFilter) {
+	if bandUserFilter(nameUser) {
+		fmt.Println("You Banned user:", nameUser)
+	} else {
+		fmt.Println("Welcome : ", nameUser)
+	}
+}
+
+func main() {
+	bannedUser := func(name string) bool {
+		return name == "goblok"
+	}
+	registerUserBanned("doni", bannedUser)
+
+	registerUserBanned("adi", func(name string) bool {
+		return name == "adi"
+	})
+}
