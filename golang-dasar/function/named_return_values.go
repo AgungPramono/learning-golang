2b4f13718807c00c8d849963a43745f9163f1568
agung <agungpermadi9@gmail.com>
2024-12-17 08:07:06 +0700

    buat folder

--- golang-dasar/function/named_return_values.go
+++ golang-dasar/function/named_return_values.go
@@ -1,21 +1,19 @@
 package main
 
-
 import "fmt"
-
 
 //named return values
 //adalah sebuah fitur di Go yang memungkinkan kita untuk mengembalikan nilai lebih dari
 //satu dari sebuah fungsi.
 //tipe data parameter harus sama semua
-func getCompleteName()(firstName, middleName,lastName string){
+func getCompleteName() (firstName, middleName, lastName string) {
 	firstName = "Agung"
 	middleName = "Satria"
 	lastName = "Budiono"
-	return firstName,middleName,lastName
+	return firstName, middleName, lastName
 }
 
-func main(){
-	a,b,c := getCompleteName();
-	fmt.Println(a,b,c)
+func main() {
+	a, b, c := getCompleteName()
+	fmt.Println(a, b, c)
 }
