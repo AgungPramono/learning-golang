db8309fd032acf44b1b726a6ee68c4926e889f6f
agung <agungpermadi9@gmail.com>
2024-12-23 13:53:10 +0700

    select channel

--- golang-goroutines/channel_test.go
+++ golang-goroutines/channel_test.go
@@ -2,6 +2,7 @@ package golang_goroutines
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -82,5 +83,58 @@ func TestChannelWithBuffer(t *testing.T) {
 	time.Sleep(2 * time.Second)
 
 	fmt.Println("Selesai mengirim data ke channel")
+
+}
+
+// range channel
+// menerima data dari channel yg jumlahnya tidak tentu
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke :" + strconv.Itoa(i)
+		}
+		defer close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menenerima data:", data)
+	}
+
+	fmt.Println("Selesai mengirim data ke channel")
+}
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	//select {
+	//case data := <-channel1:
+	//	fmt.Println("data dari channel 1:", data)
+	//case data := <-channel2:
+	//	fmt.Println("data dari channel 2:", data)
+	//}
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("data dari channel 1:", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("data dari channel 2:", data)
+			counter++
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
 
 }
