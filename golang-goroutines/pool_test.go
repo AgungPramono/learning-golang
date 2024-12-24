23f7c777833ed04d25c2153c1506d70480421e45
agung <agungpermadi9@gmail.com>
2024-12-24 10:15:45 +0700

    pool

---
+++ golang-goroutines/pool_test.go
@@ -0,0 +1,27 @@
+package golang_goroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestPool(t *testing.T) {
+	pool := sync.Pool{}
+
+	pool.Put("Golang")
+	pool.Put("Java")
+	pool.Put("C++")
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			data := pool.Get()
+			fmt.Println(data)
+			pool.Put(data) // kembalikan lagi data ke pool
+		}()
+	}
+
+	time.Sleep(3 * time.Second)
+	fmt.Println("Selesai")
+}
