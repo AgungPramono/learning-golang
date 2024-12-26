4b49324707a54f4a4326acb941ba945aadbed4e4
agung <agungpermadi9@gmail.com>
2024-12-26 09:33:20 +0700

    setting connection pooling database

--- golang-databases/database.go
+++ golang-databases/database.go
@@ -2,22 +2,20 @@ package golang_databases
 
 import (
 	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
-	"testing"
+	"time"
 )
 
-func TestEmpty(t *testing.T) {
-
-}
-
-func TestOpenConnection(t *testing.T) {
+func GetConnection() *sql.DB {
 	//buat koneksi ke database
 	db, err := sql.Open("mysql", "root:admin123@tcp(localhost:3306)/godb")
 	if err != nil {
 		panic(err)
 	}
 
-	//close koneksi setelah selesai digunakan
-	defer db.Close()
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
 
+	return db
 }
