bfb0c0334cccaba337f3ff52ddbac36461adde14
agung <agungpermadi9@gmail.com>
2024-12-30 16:04:51 +0700

    template action operator

---
+++ golang-web/template_action_test.go
@@ -0,0 +1,46 @@
+package golang_web
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TemplateIf(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/if.gohtml"))
+	t.ExecuteTemplate(writer, "if.gohtml", map[string]interface{}{
+		"Title": "Template Action",
+		"Name":  "Agung",
+	})
+}
+
+func TestTemplateActionIf(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateIf(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TemplateComparator(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/comparator.gohtml"))
+	t.ExecuteTemplate(writer, "comparator.gohtml", map[string]interface{}{
+		"Title":      "Template Action Operator",
+		"FinalValue": 40,
+	})
+}
+
+func TestTemplateActionComparator(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateComparator(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
