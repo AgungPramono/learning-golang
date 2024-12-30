9189d40ae48c8012f5419e8993b246ecfef26cb7
agung <agungpermadi9@gmail.com>
2024-12-30 08:52:15 +0700

    query parameter

--- golang-web/handler_test.go
+++ golang-web/handler_test.go
@@ -2,7 +2,9 @@ package golang_web
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -65,4 +67,24 @@ func TestRequest(t *testing.T) {
 	if err != nil {
 		return
 	}
+}
+
+func SayHello(writer http.ResponseWriter, request *http.Request) {
+	name := request.URL.Query().Get("name")
+	if name == "" {
+		fmt.Fprintf(writer, "Hello")
+	} else {
+		fmt.Fprintf(writer, "Hello %s", name)
+	}
+}
+
+func TestQueryParameter(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/hello=?name=budi", nil)
+	recorder := httptest.NewRecorder()
+
+	SayHello(recorder, request)
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
 }
