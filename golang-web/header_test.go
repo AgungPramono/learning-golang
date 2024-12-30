c099c1bcb56b01113b79acea9684b65a806ffbeb
agung <agungpermadi9@gmail.com>
2024-12-30 09:35:54 +0700

    request header

---
+++ golang-web/header_test.go
@@ -0,0 +1,27 @@
+package golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func RequestHeader(writer http.ResponseWriter, request *http.Request) {
+	contentType := request.Header.Get("content-type")
+	fmt.Fprintf(writer, contentType)
+}
+
+func TestHeader(t *testing.T) {
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080/", nil)
+	request.Header.Add("content-type", "application/json")
+
+	recorder := httptest.NewRecorder()
+
+	RequestHeader(recorder, request)
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+}
