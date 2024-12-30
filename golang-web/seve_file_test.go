a6ea5c6bc48490497251339da98ac8665c2268ab
agung <agungpermadi9@gmail.com>
2024-12-30 13:54:53 +0700

    serve file dengan embed file

--- golang-web/seve_file_test.go
+++ golang-web/seve_file_test.go
@@ -1,6 +1,8 @@
 package golang_web
 
 import (
+	_ "embed"
+	"fmt"
 	"net/http"
 	"testing"
 )
@@ -18,6 +20,32 @@ func TestServeFile(t *testing.T) {
 		Addr:    "localhost:8080",
 		Handler: http.HandlerFunc(ServeFile),
 	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+//go:embed resources/ok.html
+var resourcesOk string
+
+//go:embed resources/notfound.html
+var resourcesNotFound string
+
+func ServeFileEmbed(write http.ResponseWriter, request *http.Request) {
+	if request.URL.Query().Get("name") != "" {
+		fmt.Fprintf(write, resourcesOk)
+	} else {
+		fmt.Fprintf(write, resourcesNotFound)
+	}
+}
+
+func TestServeFileEmbed(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(ServeFileEmbed),
+	}
+
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(err)
