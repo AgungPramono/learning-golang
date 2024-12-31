505eee18a7991a35d9e0dba693340e2f4b83c014
agung <agungpermadi9@gmail.com>
2024-12-31 14:37:23 +0700

    router pattern parameter

---
+++ golang-router/router_pattern_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRouterPatternNamedParameter(t *testing.T) {
+	router.GET("/product/:id/items/:itemId", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		id := params.ByName("id")
+		itemId := params.ByName("itemId")
+		text := "Product " + id + " Item " + itemId
+		fmt.Fprint(writer, text)
+	})
+
+	//buat request
+	request, _ := http.NewRequest(http.MethodGet, BaseUrl+"/product/1/items/3", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+	if response.StatusCode == http.StatusOK {
+		body, _ := io.ReadAll(response.Body)
+		assert.Equal(t, "Product 1 Item 3", string(body))
+	}
+}
