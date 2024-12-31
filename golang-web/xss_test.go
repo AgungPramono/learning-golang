75f4e23a1f0e74446eee0e5704b3bf31384d1285
agung <agungpermadi9@gmail.com>
2024-12-31 09:22:27 +0700

    auto escape template XSS prevention

---
+++ golang-web/xss_test.go
@@ -0,0 +1,38 @@
+package golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TemplateAutoEscape(writer http.ResponseWriter, request *http.Request) {
+	myTemplate.ExecuteTemplate(writer, "post.gohtml", map[string]interface{}{
+		"Title": "Tempalate Auto Escape",
+		"Body":  "<p>Ini adalah body<% String eid = request.getParameter(\"eid\"); %></p>",
+	})
+}
+
+func TestTemplateAutEscape(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateAutoEscape(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestTemplateAutoServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: http.HandlerFunc(TemplateAutoEscape),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
