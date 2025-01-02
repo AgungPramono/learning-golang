0eb58096612eb22d43eb3ea5da1cce9300de2cc5
agung <agungpermadi9@gmail.com>
2025-01-02 16:42:15 +0700

    buat category repository impl

---
+++ golang-restful-api/repository/category_repository_impl.go
@@ -0,0 +1,70 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"golang-restful-api/helper"
+	"golang-restful-api/model/domain"
+)
+
+type CategoryRepositoryImpl struct {
+	DB  *sql.DB
+	TRX sql.Tx
+}
+
+func (repository *CategoryRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, category domain.Category) (domain.Category, error) {
+	SQL := "INSERT INTO category (name) VALUES (?)"
+	result, err := tx.ExecContext(ctx, SQL, category.Name)
+	helper.PanicIfErr(err)
+
+	id, err := result.LastInsertId()
+	helper.PanicIfErr(err)
+
+	category.Id = int(id)
+	return category, nil
+}
+
+func (repository *CategoryRepositoryImpl) Update(ctx context.Context, tx *sql.Tx, category domain.Category) (domain.Category, error) {
+	SQL := "UPDATE category SET name = ? WHERE id = ?"
+	_, err := tx.ExecContext(ctx, SQL, category.Name, category.Id)
+	helper.PanicIfErr(err)
+
+	return category, nil
+}
+
+func (repository *CategoryRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, category domain.Category) {
+	SQL := "DELETE FROM category WHERE id = ?"
+	_, err := tx.ExecContext(ctx, SQL, category.Name, category.Id)
+	helper.PanicIfErr(err)
+}
+
+func (repository *CategoryRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, categoryId int32) (domain.Category, error) {
+	SQL := "SELECT id,name FROM category WHERE id = ? limit 1"
+	rows, err := tx.QueryContext(ctx, SQL, categoryId)
+	helper.PanicIfErr(err)
+
+	category := domain.Category{}
+	if rows.Next() {
+		err := rows.Scan(&category.Id, &category.Name)
+		helper.PanicIfErr(err)
+		return category, nil
+	} else {
+		return category, errors.New("category not found")
+	}
+}
+
+func (repository *CategoryRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx) []domain.Category {
+	SQL := "SELECT * FROM category"
+	rows, err := tx.QueryContext(ctx, SQL)
+	helper.PanicIfErr(err)
+
+	var categories []domain.Category
+	for rows.Next() {
+		category := domain.Category{}
+		err := rows.Scan(&category.Id, &category.Name)
+		helper.PanicIfErr(err)
+		categories = append(categories, category)
+	}
+	return categories
+}
