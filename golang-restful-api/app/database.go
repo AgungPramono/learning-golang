0b4d62aa67d73f2c051aebf33a8e441259713bba
agung <agungpermadi9@gmail.com>
2025-01-03 11:04:26 +0700

    helper untuk koneksi ke database

---
+++ golang-restful-api/app/database.go
@@ -0,0 +1,20 @@
+package app
+
+import (
+	"database/sql"
+	"golang-restful-api/helper"
+	"time"
+)
+
+func NewDB() *sql.DB {
+	db, err := sql.Open("mysql", "root:admin123@tcp(localhost:3306)/golang_restful_api?parseTime=true")
+	helper.PanicIfErr(err)
+
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(60 * time.Minute)
+
+	return db
+
+}
