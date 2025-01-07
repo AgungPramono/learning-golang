6792f46013d96b0cac257eba5ed0422a416c2cde
agung <agungpermadi9@gmail.com>
2025-01-07 14:17:41 +0700

    log level

---
+++ golang-logging/level_test.go
@@ -0,0 +1,17 @@
+package golang_logging
+
+import (
+	"github.com/sirupsen/logrus"
+	"testing"
+)
+
+func TestLevel(t *testing.T) {
+	logger := logrus.New()
+
+	logger.Trace("Trace level")
+	logger.Debug("Debug Level")
+	logger.Info("Info Level")
+	logger.Warn("Warn Level")
+	logger.Error("Error Level")
+	logger.Fatal("Fatal Level")
+}
