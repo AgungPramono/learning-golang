a7c9c5b0c8b965c9f311989febcfbd7bf2f2d825
agung <agungpermadi9@gmail.com>
2025-01-07 10:03:12 +0700

    impl google wire ke restful-api

--- golang-restful-api/main.go
+++ golang-restful-api/main.go
@@ -1,31 +1,21 @@
 package main
 
 import (
-	"github.com/go-playground/validator/v10"
 	_ "github.com/go-sql-driver/mysql"
-	"golang-restful-api/app"
-	"golang-restful-api/controller"
-	"golang-restful-api/helper"
 	"golang-restful-api/middleware"
-	"golang-restful-api/repository"
-	"golang-restful-api/service"
 	"net/http"
 )
 
-func main() {
-	validate := validator.New()
-	db := app.NewDB()
-
-	categoryRepository := repository.NewCategoryRepository()
-	categoryService := service.NewCategoryService(categoryRepository, db, validate)
-	categoryController := controller.NewCategoryController(categoryService)
-
-	router := app.NewRouter(categoryController)
-
-	server := http.Server{
+func NewServer(authMiddleware *middleware.AuthMiddleware) *http.Server {
+	return &http.Server{
 		Addr:    "localhost:3000",
-		Handler: middleware.NewAuthMiddleware(router),
+		Handler: authMiddleware,
 	}
-	err := server.ListenAndServe()
-	helper.PanicIfErr(err)
+}
+
+func main() {
+
+	//server := InitializedServer()
+	//err := server.ListenAndServe()
+	//helper.PanicIfErr(err)
 }
