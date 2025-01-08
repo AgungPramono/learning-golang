319fd6ca20059c63bcd18a30619639890266c692
agung <agungpermadi9@gmail.com>
2025-01-08 10:54:49 +0700

    type inference: tidak menyebut type generic

--- golang-generic/type_approximation_test.go
+++ golang-generic/type_approximation_test.go
@@ -24,3 +24,10 @@ func TestMinNumber(t *testing.T) {
 	assert.Equal(t, float64(100.1), GetMin[float64](100.1, 200.2))
 	assert.Equal(t, Age(23), GetMin[Age](Age(23), Age(25)))
 }
+
+func TestTypeInference(t *testing.T) {
+	assert.Equal(t, int(100), GetMin(100, 200))
+	assert.Equal(t, int64(100), GetMin(int64(100), int64(200)))
+	assert.Equal(t, float64(100.1), GetMin(float64(100.1), float64(200.2)))
+	assert.Equal(t, Age(23), GetMin(Age(23), Age(25)))
+}
