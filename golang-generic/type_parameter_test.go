8fe57ff072c647d35176952a51ca4c4083fc9075
agung <agungpermadi9@gmail.com>
2025-01-08 09:38:05 +0700

    type parameter

--- golang-generic/type_parameter_test.go
+++ golang-generic/type_parameter_test.go
@@ -1,5 +1,21 @@
 package golang_generic
 
-func Length[T interface{}]() {
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Length[T any](param T) T {
+	fmt.Println(param)
+	return param
+}
+
+func TestSample(t *testing.T) {
+	var result string = Length[string]("agung")
+	assert.Equal(t, "agung", result)
+
+	var intResult int = Length[int](12)
+	assert.Equal(t, 12, intResult)
 
 }
