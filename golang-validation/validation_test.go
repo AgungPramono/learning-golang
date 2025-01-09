75502b460bac49a0077f4ca7561bb4e07b867144
agung <agungpermadi9@gmail.com>
2025-01-09 09:55:27 +0700

    multi tag parameter validation

--- golang-validation/validation_test.go
+++ golang-validation/validation_test.go
@@ -44,3 +44,13 @@ func TestMultiTagValidation(t *testing.T) {
 		t.Error(err.Error())
 	}
 }
+
+func TestMultiTagParameterValidation(t *testing.T) {
+	validate := validator.New()
+	username := "agung"
+
+	err := validate.Var(username, "required,alpha,min=3,max=100")
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
