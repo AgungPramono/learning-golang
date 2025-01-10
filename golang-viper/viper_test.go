4d460f346afc63c621b147c5145bede354b6539d
agung <agungpermadi9@gmail.com>
2025-01-10 10:25:09 +0700

    baca file config.yaml

--- golang-viper/viper_test.go
+++ golang-viper/viper_test.go
@@ -28,3 +28,18 @@ func TestJson(t *testing.T) {
 	assert.Equal(t, 3306, config.GetInt("database.port"))
 
 }
+
+func TestYaml(t *testing.T) {
+	config := viper2.New()
+	config.SetConfigFile("config.yaml")
+	config.AddConfigPath(".")
+
+	err := config.ReadInConfig()
+	assert.Nil(t, err)
+
+	assert.Equal(t, "belajar-golang-viper", config.GetString("app.name"))
+	assert.Equal(t, "agung", config.GetString("app.author"))
+	assert.Equal(t, "localhost", config.GetString("database.host"))
+	assert.Equal(t, true, config.GetBool("database.show_sql"))
+	assert.Equal(t, 3306, config.GetInt("database.port"))
+}
