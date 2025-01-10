3ecbcdab91c1db119d9096d8bd785107e3c5febf
agung <agungpermadi9@gmail.com>
2025-01-10 16:15:31 +0700

    config untuk fiber

--- golang-fiber/main.go
+++ golang-fiber/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"golang-fiber/helper"
+	"time"
 )
 
 func main() {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		IdleTimeout:  time.Second * 5,
+		WriteTimeout: time.Second * 5,
+		ReadTimeout:  time.Second * 5,
+	})
 
 	err := app.Listen("localhost:3000")
 	helper.PanicIfError(err)
