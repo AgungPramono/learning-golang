e03bd6b6c00f923fde630932ceef05ba42eb6ed6
agung <agungpermadi9@gmail.com>
2025-01-15 13:44:25 +0700

    enable logger

--- golang-gorm/database.go
+++ golang-gorm/database.go
@@ -3,11 +3,14 @@ package golang_gorm
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func OpenConnection() *gorm.DB {
 	dialect := mysql.Open("root:admin123@tcp(localhost:3306)/golang_gorm?charset=utf8mb4&parseTime=True&loc=Local")
-	db, err := gorm.Open(dialect, &gorm.Config{})
+	db, err := gorm.Open(dialect, &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 	if err != nil {
 		panic(err)
 	}
