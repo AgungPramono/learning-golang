dedb1ac5c24f1df4f0df22efc399fbd261adb4a9
agung <agungpermadi9@gmail.com>
2025-01-15 10:48:59 +0700

    gorm model

--- golang-gorm/skema_database.sql
+++ golang-gorm/skema_database.sql
@@ -5,3 +5,14 @@ create table sample(
 )engine =InnoDB;
 
 select * from sample;
+
+create table users(
+    id varchar(100) not null ,
+    password varchar(100)not null ,
+    name varchar(100)not null ,
+    create_at timestamp not null default current_timestamp,
+    update_at timestamp not null default current_timestamp on update current_timestamp,
+    primary key (id)
+)engine =InnoDB;
+
+select * from users
---
+++ golang-gorm/user.go
@@ -0,0 +1,16 @@
+package golang_gorm
+
+import "time"
+
+type User struct {
+	Id        string    `gorm:"primary_key;column:id"`
+	Password  string    `gorm:"column:password"`
+	Name      string    `gorm:"column:name"`
+	CreatedAt time.Time `gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt time.Time `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
+}
+
+// override nama table user
+func (u *User) TableName() string {
+	return "users"
+}
