f53764507925eb0e5e989f9ecd413de895d0895a
agung <agungpermadi9@gmail.com>
2025-01-15 10:08:22 +0700

    execute raw sql insert

--- golang-gorm/gorm_test.go
+++ golang-gorm/gorm_test.go
@@ -9,3 +9,17 @@ func TestOpenConnection(t *testing.T) {
 	connection := OpenConnection()
 	assert.NotNil(t, connection)
 }
+
+func TestExecuteRawSql(t *testing.T) {
+	err := OpenConnection().Exec("insert into sample(id, name) value (?,?)", "1", "agung").Error
+	assert.Nil(t, err)
+
+	err = OpenConnection().Exec("insert into sample(id, name) value (?,?)", "2", "joko").Error
+	assert.Nil(t, err)
+
+	err = OpenConnection().Exec("insert into sample(id, name) value (?,?)", "3", "ahmad").Error
+	assert.Nil(t, err)
+
+	err = OpenConnection().Exec("insert into sample(id, name) value (?,?)", "4", "budi").Error
+	assert.Nil(t, err)
+}
