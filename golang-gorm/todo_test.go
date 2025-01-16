5ca2878289a0443e1665d350d8b6090ba9b8c24b
agung <agungpermadi9@gmail.com>
2025-01-16 13:31:53 +0700

    gorm :Unscoped Soft Delete

--- golang-gorm/todo_test.go
+++ golang-gorm/todo_test.go
@@ -1,15 +1,16 @@
 package golang_gorm
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestDeleteTodo(t *testing.T) {
 	todo := Todo{
-		UserId:      "3",
-		Title:       "Todo 3",
-		Description: "Description 3",
+		UserId:      "6",
+		Title:       "Todo 6",
+		Description: "Description 6",
 	}
 
 	err := Db().Create(&todo).Error
@@ -25,6 +26,24 @@ func TestDeleteTodo(t *testing.T) {
 	assert.Equal(t, 0, len(todos))
 
 	var deleteTodo Todo
-	result = Db().Take(&deleteTodo, "id=?", "3")
+	result = Db().Take(&deleteTodo, "id=?", "6")
 	assert.NotNil(t, result.Error)
+}
+
+/*
+*
+query dengan mengabaikan data yang sudah di hapus dengan metode Soft Delete
+*/
+func TestUnscoped(t *testing.T) {
+	var todo Todo
+	err := Db().Unscoped().First(&todo, "id=?", "6").Error
+	assert.Nil(t, err)
+	fmt.Println(todo)
+
+	err = Db().Unscoped().Delete(&todo).Error
+	assert.Nil(t, err)
+
+	var todos []Todo
+	err = Db().Unscoped().Find(&todos).Error
+	assert.Nil(t, err)
 }
