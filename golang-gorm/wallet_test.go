50beb84479ea4630275282acc7fc6cbe0433a52a
agung <agungpermadi9@gmail.com>
2025-01-16 14:30:55 +0700

    gorm :one to one preload query relation

---
+++ golang-gorm/wallet_test.go
@@ -0,0 +1,25 @@
+package golang_gorm
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCreateWallet(t *testing.T) {
+	wallet := Wallet{
+		Id:      "1",
+		UserId:  "3",
+		Balance: 40_000_000,
+	}
+	err := Db().Create(&wallet).Error
+	assert.Nil(t, err)
+}
+
+func TestRetrieveRelationUserWallet(t *testing.T) {
+	var user User
+	result := Db().Model(&User{}).Preload("Wallet").Take(&user, "id=?", "3")
+	assert.Nil(t, result.Error)
+
+	assert.Equal(t, "3", user.Id)
+	assert.Equal(t, "3", user.Wallet.Id)
+}
