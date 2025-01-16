dc6e94f9dda5346269a30aacc13f629b77171017
agung <agungpermadi9@gmail.com>
2025-01-16 16:18:55 +0700

    gorm :select relation belongsto many to one : Preload-Join

--- golang-gorm/user_address_test.go
+++ golang-gorm/user_address_test.go
@@ -1,6 +1,7 @@
 package golang_gorm
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"golang-gorm/model"
 	"testing"
@@ -8,10 +9,10 @@ import (
 
 func TestCreateUserAddresses(t *testing.T) {
 	user := model.User{
-		Id:       "22",
-		Password: "rahasia 22",
+		Id:       "20",
+		Password: "rahasia 20",
 		Name: model.Name{
-			FirstName: "Ahmad 22",
+			FirstName: "Jumadi 20",
 		},
 		Wallet: model.Wallet{
 			Id:      "3",
@@ -20,28 +21,41 @@ func TestCreateUserAddresses(t *testing.T) {
 		},
 		Addresses: []model.Address{
 			{
-				UserId:  "22",
-				Address: "Surabaya",
+				UserId:  "20",
+				Address: "Madiun",
 			},
 			{
-				UserId:  "22",
-				Address: "Kediri",
+				UserId:  "20",
+				Address: "Nganjuk",
 			},
 		},
 	}
 
-	err := Db().Create(&user).Error
+	err := Db().Save(&user).Error
 	assert.Nil(t, err)
 }
 
 func TestPreloadJoinOneToMany(t *testing.T) {
 	var userPreload []model.User
-	err := Db().Model(&model.User{}).Preload("Address").Joins("Wallet").Find(&userPreload).Error
+	err := Db().Model(&model.User{}).Preload("Addresses").Joins("Wallet").Find(&userPreload).Error
 	assert.Nil(t, err)
 }
 
 func TestTakePreloadJoinOneToMany(t *testing.T) {
 	var user model.User
 	err := Db().Model(&model.User{}).Preload("Addresses").Joins("Wallet").Take(&user, "users.id=?", "22").Error
+	assert.Nil(t, err)
+}
+
+func TestBelongsToAddressUser(t *testing.T) {
+	fmt.Println("Preload")
+	var addresses []model.Address
+	err := Db().Model(&model.Address{}).Preload("User").Find(&addresses).Error
+	assert.Nil(t, err)
+	fmt.Println(len(addresses))
+
+	fmt.Println("Join")
+	addresses = []model.Address{}
+	err = Db().Model(&model.Address{}).Joins("User").Find(&addresses).Error
 	assert.Nil(t, err)
 }
